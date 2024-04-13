a5ee2555ed84c3e871b720a14a530e3324d53916
Elizaveta Khlebnikova <elizavetakhlebnikova@gmail.com>
2024-04-13 21:36:40 +0300

    Users/View tests updated

--- pets/tests.py
+++ pets/tests.py
@@ -14,30 +14,51 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, 'pets/index.html')
         self.assertEqual(response.context_data['title'], 'HappyVeganShelter')
+        #IndexViewTestCase.test_view
 
 
 class PetListTestCase(TestCase):
     fixtures = ['pet_category.json', 'pet_status.json', 'pets.json']
 
     def setUp(self):
-        self.paginate_by = 3
+        self.paginate_by = 9
 
-    def test_list(self):
-        page = left = 0
-        right = self.paginate_by
-        data_len = len(Pet.objects.all())
-
-        while data_len > 0:
-            page += 1
-            path = reverse('pets:paginator', kwargs={'page': page})
-            response = self.client.get(path)
-            self.assertQuerysetEqual(response.context_data.get('object_list'), Pet.objects.all()[left:right],
-                                     ordered=False)
-            self.assertEqual(response.status_code, HTTPStatus.OK)
-            self.assertTemplateUsed(response, 'pets/pets.html')
-            data_len -= self.paginate_by
-            left = right
-            right += self.paginate_by
+    # def test_list(self):
+    #     path = reverse('pets:index')
+    #     response = self.client.get(path)
+    #     self.assertEqual(response, response)
+    #
+    # def test_filter_by_category(self):
+    #     data_len = len(Pet.objects.filter(category=1))
+    #     page = left = 0
+    #     right = self.paginate_by
+    #     response = self.client.get(reverse('pets:index'), kwargs={'category': '1'})
+    #     exp_response = Pet.objects.filter(category=1)
+    #     self.assertQuerysetEqual(response.context_data.get('object_list'), exp_response,
+    #                              ordered=False)
+    #
+    #     while data_len > 0:
+    #         page += 1
+    #         response = self.client.get(reverse('pets:index'), kwargs={'category_id': '1', 'page': page})
+    #         exp_response = Pet.objects.filter(category=1)[left:right]
+    #         self.assertQuerysetEqual(response.context_data.get('object_list'), exp_response,
+    #                                  ordered=False)
+    #         self.assertEqual(response.status_code, HTTPStatus.OK)
+    #         self.assertTemplateUsed(response, 'pets/pets.html')
+    #         data_len -= self.paginate_by
+    #         left = right
+    #         right += self.paginate_by
+    #
+    # def test_filter_by_gender(self):
+    #     path = reverse('pets:index')
+    #     response = self.client.get(path + '?category=-&gender=m&status=-')
+    #     # Проверьте, соответствуют ли результаты заданному фильтру по полу.
+    #     self.assertEqual(response.status_code, HTTPStatus.OK)
+    #
+    # def test_filter_by_status(self):
+    #     response = self.client.get(reverse('pets:index'), kwargs={'status': '1'})
+    #     # Проверьте, соответствуют ли результаты заданному фильтру по статусу.
+    #     self.assertTrue('условие проверки')
 
     def test_category(self):
 
--- users/tests.py
+++ users/tests.py
@@ -1,12 +1,67 @@
 from datetime import timedelta
 from http import HTTPStatus
+import uuid
 
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils.timezone import now
+from django.conf import settings
+from django.test.utils import override_settings
+
+from unittest.mock import patch
 
 from users.forms import UserLoginForm, UserProfileForm
 from users.models import EmailVerification, User
+from .services.verification import send_verification_email, create_and_send_email_verification_object
+from .tasks import send_email_verification
+
+
+class TestEmailServices(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='testuser', email='test@example.com')
+        self.expiration = now() + timedelta(hours=48)
+        self.record = EmailVerification.objects.create(user=self.user, code=uuid.uuid4(), expiration=self.expiration)
+
+    @patch('users.services.verification.send_mail')
+    def test_send_verification_email(self, mock_send_mail):
+        # проверка однократного вызова функции send_mail
+        send_verification_email(self.record)
+
+        # ожидаемое содержание письма:
+        link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.record.code})
+        verification_link = f'{settings.DOMAIN_NAME}{link}'
+        expected_subject = f'Подтверждение учётной записи для {self.user}'
+        expected_message_start = 'Для подтверждения учетной записи для {} перейдите по ссылке {}, {}'.format(
+            self.user.username,
+            verification_link,
+            self.expiration
+        )
+
+        # Проверка факта отправки письма
+        self.assertTrue(mock_send_mail.called)
+        mock_send_mail.assert_called_once()
+        # Получаем аргументы, с которыми была вызвана mock-функция
+        sent_args = mock_send_mail.call_args
+        # Проверяем тему письма
+        self.assertEqual(sent_args[1]['subject'], expected_subject)
+        # Проверяем, совпадает ли текст сообщения с ожидаемым содержимым письма
+        self.assertTrue(sent_args[1]['message'].startswith(expected_message_start))
+        print(len(EmailVerification.objects.all()))
+
+    @patch('users.services.verification.send_verification_email')
+    def test_create_and_send_email_verification_object(self, mock_send_verification_email):
+        create_and_send_email_verification_object(self.user.id)
+        # так как в setUp уже был создан один объект EmailVerification, то сейчас мы ожидаем добавление второго
+        self.assertEqual(EmailVerification.objects.count(), 2)
+        # проверяем запуск функции отправки письма
+        mock_send_verification_email.assert_called_once()
+        # проверяем корректность данных объекта EmailVerification
+        email_verification = EmailVerification.objects.filter(user__username=self.user.username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification[1].expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
 
 
 class UserRegistrationViewTestCase(TestCase):
@@ -28,24 +83,18 @@ class UserRegistrationViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'HappyVeganShelter - Регистрация')
         self.assertTemplateUsed(response, 'users/registration.html')
-        # UserRegistrationViewTestCase.test_user_registration_get
 
-    def test_user_registration_post_success(self):
+    @patch('users.tasks.send_email_verification.delay')
+    def test_user_registration_post_success(self, mock_send_email):
         response = self.client.post(self.path, self.data)
         username = self.data['username']
 
-        # check creating of user
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, reverse('users:login'), fetch_redirect_response=False)
         self.assertTrue(User.objects.filter(username=username).exists())
+        self.assertEqual(User.objects.count(), 1)
 
         # check creating of email verification
-        email_verification = EmailVerification.objects.filter(user__username=username)
-        self.assertTrue(email_verification.exists())
-        self.assertEqual(
-            email_verification[0].expiration.date(),
-            (now() + timedelta(hours=48)).date()
-        )
 
     def test_user_registration_post_name_error(self):
         username = self.data['username']
@@ -54,7 +103,6 @@ class UserRegistrationViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
-        # UserRegistrationViewTestCase.test_user_registration_post_error
 
     def test_user_registration_post_email_error(self):
         email = self.data['email']
@@ -122,3 +170,4 @@ class UserProfileViewTestCase(TestCase):
         self.assertIsInstance(response.context['form'], UserProfileForm)
         # проверить, что в ответе используется правильный шаблон
         self.assertTemplateUsed(response, 'users/profile.html')
+        # UserProfileViewTestCase.test_profile_view
